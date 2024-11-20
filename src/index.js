5e13f1c240a8744ed6642b459e3de43080da8a55
Pascal Brand <pascal.brand38@gmail.com>
2024-11-20 17:13:21 +0100

    astro-splide: SplideTrack
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@
 // MIT License
 
 export { default as Splide } from './Splide.astro';
+export { default as SplideTrack } from './SplideTrack.astro';
 export { default as SplideSlide } from './SplideSlide.astro';
