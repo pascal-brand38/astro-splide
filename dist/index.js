65c97661c3f5d32ba1fa0209c3ed83bff05db1ce
Pascal Brand <pascal.brand38@gmail.com>
2024-11-21 11:36:28 +0100

    astro-splide: TS and dist
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Copyright (c) Pascal Brand
+// MIT License
+
+// Copyright (c) Pascal Brand
+// MIT License
+
+export { default as Splide } from './Splide.astro';
+export { default as SplideTrack } from './SplideTrack.astro';
+export { default as SplideSlide } from './SplideSlide.astro';
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// Copyright (c) Pascal Brand
+// MIT License
