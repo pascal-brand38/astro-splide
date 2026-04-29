7748e7c250d32b64abe5cf23664ba4ac5f0008cc
Pascal Brand <pascal.brand38@gmail.com>
2026-04-29 08:46:51 +0200

    feat: export splide API: types
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

---
+++ splide.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Pascal Brand
+// MIT License
+//
+// export splide API
+// to be used with
+//   import type { Options } from 'astro-splide/splide'
+
+export type * from '@splidejs/splide';
